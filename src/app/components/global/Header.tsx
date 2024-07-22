6b9b6b829783d22d797ed346dff10a2b6ea11f69
Dubjay18 <jejeniyi7@gmail.com>
2024-07-22 23:31:39 +0100

    chore: header & hero ui changes

--- src/app/components/global/Header.tsx
+++ src/app/components/global/Header.tsx
@@ -1,55 +1,47 @@
-import React from 'react';
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Search } from "lucide-react";
+import { roboto, robotoSerif } from "@/lib/fonts";
 import Image from "next/image";
 
-import Link from "next/link";
-import {Button} from "@/components/ui/button";
-import {Search} from "lucide-react";
-import {roboto, robotoSerif} from "@/lib/fonts";
-
-
 const Header = () => {
-    return (
-        <header className={" "}>
-            <div className={"bg-[#1e1e1e] w-full py-2 "}>
-
-                <div className={"text-white flex items-center justify-between container "}>
-                    <div className={" flex items-center gap-3"}>
-                        <Image src={"/oau_logo.svg"} alt={"OAU Logo"} width={20} height={20}/>
-                        <h1 className={robotoSerif.className}>OBAFEMI AWOLOWO UNIVERSITY</h1>
-                    </div>
-                    <Link href={"https://oauife.edu.ng/"} target={"_blank"}
-                          className={`text-white underline ${roboto.className}`}>
-                        OAUIFE.EDU.NG
-                    </Link>
-                </div>
-            </div>
-            <div className={" container py-2 flex items-center justify-between"}>
-                <h1 className={"text-primary md:text-3xl text-xl font-medium"}>Office of Institutional <br/> Research &
-                    Analytics
-                </h1>
-                <div className={`flex items-center max-md:hidden lg:gap-8 gap-4 ${roboto.className}`}>
-                    <Link href={"/"} className={"text-[#1e1e1e] font-normal"}>
-                        Home
-                    </Link>
-                    <Link href={"/"} className={"text-[#1e1e1e] font-normal"}>
-                        Articles
-                    </Link>
-                    <Link href={"/"} className={"text-[#1e1e1e] font-normal"}>
-                        Data
-                    </Link> <Link href={"/"} className={"text-[#1e1e1e] font-normal"}>
-                    References
-                </Link>
-                    <Link href={"/"} className={"text-[#1e1e1e] font-normal"}>
-                        FAQs
-                    </Link>
-
-                </div>
-                <div>
-                    <Button><Search className={"text-white"} size={24}/> </Button>
-                </div>
-            </div>
-        </header>
-    );
+  return (
+    <header className={" pt-5"}>
+      <div className={" container py-2 flex items-center justify-between"}>
+        <div className={" flex items-center gap-3"}>
+          <Image
+            src={"/oau_logo.svg"}
+            alt={"OAU Logo"}
+            width={40}
+            height={40}
+          />
+          <h1
+            className={`${robotoSerif.className} text-primary font-semibold text-xl`}
+          >
+            OAUIR
+          </h1>
+        </div>
+        <div
+          className={`flex items-center max-md:hidden lg:gap-8 gap-4 ${roboto.className}`}
+        >
+          <Link href={"/"} className={"text-[#1e1e1e] font-normal"}>
+            Home
+          </Link>
+          <Link href={"/"} className={"text-[#1e1e1e] font-normal"}>
+            Data
+          </Link>
+          <Link href={"/"} className={"text-[#1e1e1e] font-normal"}>
+            Request Data
+          </Link>
+        </div>
+        <div>
+          <Button>
+            <Search className={"text-white"} size={24} />{" "}
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/app/components/home/Hero.section.tsx
+++ src/app/components/home/Hero.section.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
 import Image from "next/image";
-import {robotoSlab} from "@/lib/fonts";
+import { robotoSlab } from "@/lib/fonts";
+import { Button } from "@/components/ui/button";
 
 const HeroSection = () => {
-    return (
-        <div className={"container flex max-md:flex-col items-center justify-between mt-20"}>
-            <div className={"max-w-lg"}>
-                <h3 className={`text-primary md:text-4xl text-2xl ${robotoSlab.className}`}>
-                    Advancing Knowledge Through Research
-                </h3>
-                <p className={"text-cusGray mt-2 font-medium text-lg max-md:mb-10"}>
-                    Explore a world of research data at OAU&apos;s Institute of Research & Data Analytics. We transform
-                    complex datasets into user-friendly resources, fueling innovation and public understanding.
-                </p>
-            </div>
-            <div>
-                <Image src={"/home/landscape.png"} alt={"OAU_landscape"} className={"max-md:max-w-md max-sm:max-w-sm"}
-                       height={600}
-                       width={600}/>
-            </div>
+  return (
+    <div className={"container flex max-md:flex-col  justify-between mt-20"}>
+      <div>
+        <Image
+          src={"/home/landscape.png"}
+          alt={"OAU_landscape"}
+          className={"max-md:max-w-md max-sm:max-w-sm"}
+          height={600}
+          width={600}
+        />
+      </div>
+      <div className={"max-w-xl"}>
+        <p className="text-lg text-semibold">Introduction</p>
+        <h3
+          className={`text-primary md:text-4xl text-2xl ${robotoSlab.className}`}
+        >
+          Advancing Knowledge Through Research
+        </h3>
+        <p className={"text-cusGray mt-3 font-medium text-lg max-md:mb-10"}>
+          Explore a world of research data at OAU&apos;s Institute of Research &
+          Data Analytics. We transform complex datasets into user-friendly
+          resources, fueling innovation and public understanding.
+        </p>
+        <br />
+        <div className="flex items-center gap-6">
+          <Button variant={"outline"} size={"lg"}>
+            Go to Dashboard
+          </Button>
+          <Button variant={"outline"} size={"lg"}>
+            Quick Start
+          </Button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default HeroSection;
