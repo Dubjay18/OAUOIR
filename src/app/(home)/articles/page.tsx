7ad0c5a5fbf162a86b67aeaed39d02bafe27a6d2
Dubjay18 <jejeniyi7@gmail.com>
2024-08-23 23:51:36 +0100

    feat: article init

--- src/app/(home)/articles/page.tsx
+++ src/app/(home)/articles/page.tsx
@@ -1,5 +1,57 @@
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function page() {
-  return <div>aritlcles</div>;
+  const articles = [
+    {
+      title: "Article 1",
+      coverImage: "https://via.placeholder.com/150",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      author: "John Doe",
+      date: "2022-01-01",
+    },
+    {
+      title: "Article 2",
+      coverImage: "https://via.placeholder.com/150",
+      content:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem.",
+      author: "Jane Smith",
+      date: "2022-01-02",
+    },
+    {
+      title: "Article 3",
+      coverImage: "https://via.placeholder.com/150",
+      content:
+        "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.",
+      author: "Robert Johnson",
+      date: "2022-01-03",
+    },
+  ];
+
+  return (
+    <div className="min-h-screen container">
+      <div className="grid xl:grid-cols-4 gap-5 md:grid-cols-3 sm:grid-cols-2 py-10">
+        {articles.map((article, index) => (
+          <Card key={index}>
+            <CardHeader></CardHeader>
+            <CardContent>
+              <img src={article.coverImage} alt="cover image" />
+            </CardContent>
+            <CardFooter className="flex flex-col items-start">
+              <CardTitle>{article.title}</CardTitle>
+              <br />
+              <CardDescription>{article.author}</CardDescription>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
 }
